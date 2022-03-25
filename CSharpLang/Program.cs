4a47455b0ff8af4744dab7999b2f4a5cef46533d
byungjin <byungjin.dev@gmail.com>
2022-03-25 19:02:03 +0900

    build c# environment

---
+++ CSharpLang/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using WMPLib;
+
+namespace CSharpLang{
+    class Program{
+        static void Main(string[] strs){
+            WindowsMediaPlayer player = new WindowsMediaPlayer();
+
+            Console.WriteLine("hello");
+        }
+    }
+}
